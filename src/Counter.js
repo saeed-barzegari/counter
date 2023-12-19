da25964f0552941751e738b71fc3eab6e1285fd4
Saeed <74924533+saeed-barzegari@users.noreply.github.com>
2023-12-19 19:26:48 +0000

    Increment and Decrement

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 const Counter = () => {
     const [count, setCount] = useState(0);
+    return (
+        <div>
+        <h1>Count: {count}</h1>
+        </div>
+    );
 };
 export default Counter;
